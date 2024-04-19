160cac8ae8da7583942d2ea6be38541a0548cc7f
Denys Chernousov <blackInOwls@gmail.com>
2024-04-19 17:07:54 +0300

    new cms server

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -11,10 +11,10 @@ const AboutPage = () => {
     return (
         <div className={"flex flex-col flex-grow"}>
             <Heading>About</Heading>
-            <div className={"flex flex-col flex-grow justify-center"}>
-                <div className={"flex text-xl justify-center"}>
+            <div className={"flex flex-col flex-grow justify-center items-center"}>
+                <div className={"flex text-xl items-center"}>
                     <div className={"mr-2"}> Made by</div>
-                    <Link className={"bg-teal-700 accent-teal-700"} href={"https://chernousov.vercel.app/"}>Chernousov
+                    <Link className={"bg-teal-700 accent-teal-700 p-1 text-white"} href={"https://chernousov.vercel.app/"}>Chernousov
                         Denis</Link>
                 </div>
             </div>
